01442ff2db075f10e5f40145174370491c3ee524
d kloimwieder <dkkloimwieder@gmail.com>
2022-07-25 18:20:33 -0400

    bump deps

--- src/Circles.tsx
+++ src/Circles.tsx
@@ -1,15 +1,36 @@
-import { createEffect, Index } from 'solid-js';
+import { createEffect, Index, onMount } from 'solid-js';
 import { createStore } from 'solid-js/store';
-import { select } from 'd3';
+//import { select } from 'd3';
 import { scaleLinear } from 'd3';
-//import { setAttribute } from 'solid-js/web';
+//import '/src/styles.css';
 
 interface Circle {
-    x: number;
-    y: number;
+    cx: number;
+    cy: number;
     r: number;
-    colour: number;
+    fill: number;
 }
+const Animated = () => {
+    let el: any;
+
+    onMount(() => {
+        el.animate(
+            [
+                { transform: 'translateX(500px)' },
+                { transform: 'translateX(100px)' },
+            ],
+            {
+                duration: 500,
+                iterations: Infinity,
+                easing: 'linear',
+                fill: 'forwards',
+            }
+        );
+    });
+
+    return <div ref={el}>hello</div>;
+};
+
 const Circles = () => {
     let width: number = 1000;
     let height: number = 1000;
@@ -20,81 +41,111 @@ const Circles = () => {
         let data: Circle[] = [];
         for (let i = 0; i < numItems; i++) {
             data.push({
-                x: Math.random(),
-                y: Math.random(),
+                cx: Math.random(),
+                cy: Math.random(),
                 r: Math.random(),
-                colour: Math.floor(colours.length * Math.random()),
+                fill: Math.floor(colours.length * Math.random()),
             });
         }
         return data;
     };
-    const [circs, setCircs] = createStore({ circles: getData(100) });
-    //const [circles, setCircles] = createSignal(getData());
-    let computedSVGNode: any;
-    let circleEl: SVGCircleElement[] = [];
-    createEffect(() => {
-        let maxRadius = 50;
-        let xScale = scaleLinear().domain([0, 1]).range([0, width]);
-        let yScale = scaleLinear().domain([0, 1]).range([0, height]);
-        let rScale = scaleLinear().domain([0, 1]).range([0, maxRadius]);
-        select(svgEl)
-            .selectAll('circle')
-            .data(circs.circles)
-            //.transition()
-            //.duration(100)
-            .attr('cx', (d) => xScale(d.x))
-            .attr('cy', (d) => yScale(d.y))
-            .attr('r', (d) => rScale(d.r))
-            .style('fill', (d) => colours[d.colour]);
-    });
-    //createEffect(() => {
-    //    select(svgEl)
-    //        .selectAll('circle')
-    //        .data(circs.circles)
-    //        .join()
-    //        .transition()
-    //        .duration(1000);
-    //});
-    //     let maxRadius = 50;
-    //     let xScale = scaleLinear().domain([0, 1]).range([0, width]);
-    //     let yScale = scaleLinear().domain([0, 1]).range([0, height]);
-    //     let rScale = scaleLinear().domain([0, 1]).range([0, maxRadius]);
+    const [circs, setCircs] = createStore({ circles: getData(10) });
+    let circleEle: SVGCircleElement[] = [];
+    let maxRadius = 50;
+    let xScale = scaleLinear().domain([0, 1]).range([0, width]);
+    let yScale = scaleLinear().domain([0, 1]).range([0, height]);
+    let rScale = scaleLinear().domain([0, 1]).range([0, maxRadius]);
+    // createEffect(() => {
 
-    // createMemo(() =>
-    //     circs.circles.map((n, i) => {
-    //         select(circleEl[i])
-    //             .datum(n)
-    //             //.transition()
-    //             //.duration(100)
-    //             .attr('cx', (d) => xScale(d.x))
-    //             .attr('cy', (d) => yScale(d.y))
-    //             .attr('r', (d) => rScale(d.r))
-    //             .style('fill', (d) => colours[d.colour]);
-    //     })
-    // );
-    //  computedSVGNode = select(svgEl)
-    //      .selectAll('circle')
-    //      .datum(circs.circles)
-    //      .transition()
-    //      .duration(1000);
-    //});
-    //         .attr('cx', (d) => xScale(d.x))
-    //         .attr('cy', (d) => yScale(d.y))
-    //         .attr('r', (d) => rScale(d.r))
-    //         .style('fill', (d) => colours[d.colour]);
-
-    //     // console.log(computedSVGNode, svgEl);
-    //     // circs.circles.map((n, i) => {
-    //     //     select(computedSVGNode[i])
-    //     //         .datum(circs.circles[i])
-    //     //         .attr('cx', (d) => xScale(d.x))
-    //     //         .attr('cy', (d) => yScale(d.y))
-    //     //         .attr('r', (d) => rScale(d.r))
-    //     //         .style('fill', (d) => colours[d.colour]);
-    //     // });
+    //     for (let i = 0; i < circleEle.length; i++) {
+    //         // const c = document.createElementNS(
+    //         //     'http://www.w3.org/2000/svg',
+    //         //     'circle'
+    //         // );
+    //         circleEle[i].setAttribute(
+    //             'cx',
+    //             xScale(circs.circles[i].cx).toString()
+    //         );
+    //         circleEle[i].setAttribute(
+    //             'cy',
+    //             yScale(circs.circles[i].cy).toString()
+    //         );
+    //         circleEle[i].setAttribute(
+    //             'r',
+    //             rScale(circs.circles[i].r).toString()
+    //         );
+    //         circleEle[i].style.fill = colours[circs.circles[i].fill];
+    //         //svgEl.append(c);
+    //     }
+    //     // select(svgEl)
+    //     //     .selectAll('circle')
+    //     //     .data(circs.circles)
+    //     //     //   .transition()
+    //     //     //   .duration(1000)
+    //     //     .attr('cx', (d) => xScale(d.x))
+    //     //     .attr('cy', (d) => yScale(d.y))
+    //     //     .attr('r', (d) => rScale(d.r))
+    //     //     .style('fill', (d) => colours[d.colour]);
     // });
+    interface Spring {
+        position: number;
+        velocity: number;
+        stiffness: number;
+        damping: number;
+    }
+    const sprong = (spr: Spring, len: number) => {
+        const { position, velocity, stiffness, damping } = spr;
+        const d = position - len;
+        const sForce = -stiffness * d;
+        const dForce = sForce + -damping * velocity;
+        const acc = dForce; // * mass=1
+        const refresh = 1 / 60;
+
+        const newVelocity = velocity + acc * refresh;
+        const newPosition = position + newVelocity * refresh;
+
+        return { position: newPosition, velocity: newVelocity };
+    };
+
+    const animatePos = (spr: Spring, len: number, time: number) => {
+        let { position, velocity, stiffness } = spr;
+        while (time < 1 - 1 / 60) {
+            ({ position, velocity } = sprong(spr, len));
+            console.log(position);
+        }
+        position = len;
+    };
+    let running = 0;
+    let total = 1;
+    while (running < total) {
+        animatePos(
+            { position: 100, velocity: 100, stiffness: 15, damping: 50 },
+            100,
+            running
+        );
+        running += 1 / 60;
+    }
+
+    const ani = (ele: Element) => {
+        const x = Math.floor(Math.random() * 500 - 250);
+        const y = Math.floor(Math.random() * 500 - 250);
+        ele.animate(
+            [
+                { transform: `translate(${x}px, ${y}px)` },
+                //{ transform: `translateY(${y}px)` },
+            ],
+            {
+                duration: 500,
+                //iterations: Infinity,
+                easing: 'ease-in-out',
+                fill: 'both',
+            }
+        );
+    };
+
     return (
         <div>
+            <Animated></Animated>
             <svg width={width} height={height} ref={svgEl}>
                 <Index each={circs.circles}>
                     {(d, i) => {
@@ -108,28 +159,42 @@ const Circles = () => {
                         let rScale = scaleLinear()
                             .domain([0, 1])
                             .range([0, maxRadius]);*/
-                        return (
+                        const ci = (
                             <circle
-                                onClick={() => {
+                                //class="circ"
+                                onClick={(e) => {
+                                    ani(e.currentTarget);
                                     //select(circleEl[i]).style('fill', 'red');
-                                    setCircs('circles', i, 'colour', 0);
+                                    //setCircs('circles', i, 'fill', 0);
                                 }}
-                                cx={1}
-                                cy={1}
-                                r={0}
+                                cx={xScale(circs.circles[i].cx)}
+                                cy={yScale(circs.circles[i].cy)}
+                                r={rScale(circs.circles[i].r)}
                                 style={{
-                                    fill: colours[d().colour],
+                                    fill: colours[circs.circles[i].fill],
                                 }}
-                                ref={circleEl[i]}
-                                // cx={xScale(d().x)}
-                                // cy={yScale(d().y)}
-                                // r={rScale(d().r)}
-                                // style={{
-                                //     fill: colours[d().colour],
-                                // }}
-                                // ref={circleEl[i]}
+                                ref={circleEle[i]}
                             />
-                        );
+                        ) as Element;
+
+                        return ci;
+
+                        //     return (
+                        //         <circle
+                        //             //class="circ"
+                        //             onClick={() => {
+                        //                 //select(circleEl[i]).style('fill', 'red');
+                        //                 setCircs('circles', i, 'fill', 0);
+                        //             }}
+                        //             cx={xScale(circs.circles[i].cx)}
+                        //             cy={yScale(circs.circles[i].cy)}
+                        //             r={rScale(circs.circles[i].r)}
+                        //             style={{
+                        //                 fill: colours[circs.circles[i].fill],
+                        //             }}
+                        //             ref={circleEle[i]}
+                        //         ></circle>
+                        //     );
                     }}
                 </Index>
             </svg>
